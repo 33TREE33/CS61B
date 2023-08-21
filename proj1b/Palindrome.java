6e8ae82a0a7d4ed2ea5977dd93aa1c7e8f5db0f1
tree <tree@cs61b.com>
2023-08-22 01:08:00 +0800

    completed proj1b

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -4,6 +4,32 @@ public class Palindrome {
         for (int i = 0; i < word.length(); ++i) {
             res.addLast(word.charAt(i));
         }
-        return null;
+        return res;
+    }
+
+    public boolean isPalindrome(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }
+        Deque<Character> charDeque = wordToDeque(word);
+        for (int i = 0; i < charDeque.size() / 2; i++) {
+            if (charDeque.removeFirst() != charDeque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        if (word.length() <= 1) {
+            return true;
+        }
+        Deque<Character> charDeque = wordToDeque(word);
+        for (int i = 0; i < charDeque.size() / 2; i++) {
+            if (!cc.equalChars(charDeque.removeFirst(), charDeque.removeLast())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -1,21 +1,16 @@
-//import org.junit.Test;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-public class TestOffByOne {
-
-    // You must use this CharacterComparator and not instantiate
-    // new ones, or the autograder might be upset.
-    static CharacterComparator offByOne = new OffByOne();
+public class TestOffByN {
+    static CharacterComparator offByOne = new OffByN(2);
 
     // Your tests go here.
     //Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
     @Test
     public void testEqualChars() {
-        assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('b', 'c'));
-        assertTrue(offByOne.equalChars('e', 'd'));
+        assertTrue(offByOne.equalChars('a', 'c'));
+        assertTrue(offByOne.equalChars('b', 'd'));
+        assertTrue(offByOne.equalChars('e', 'g'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('a', 'g'));
         assertFalse(offByOne.equalChars('a', 'm'));
